ef2a12cc994cd0ca3835464e3621e415e5492e22
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2023-12-16 16:31:21 +0100

    feat(client): open connection and read 1024 messages

--- client/main.go
+++ client/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("Lauched server")
+	fmt.Println("Launched client")
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connected to server")
+	for i := 1; i <= 1024; i++ {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(buf[:n])
+	}
+
 }
