cae79173784ca401769f49bfdbf67075652160ee
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2023-12-19 14:31:50 +0100

    feat(client): send file size, content, extension size and extension

--- client/main.go
+++ client/main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
+	"math"
 	"net"
+	"os"
+	"path/filepath"
 )
+
+var payloadFolder = "./payload/"
+
+// var payloadFileName = "loremipsum.txt"
+// var payloadFileName = "textfile"
+var payloadFileName = "IMG_4905.jpg"
+
+var packetSize = 1024
 
 func main() {
 	fmt.Println("Launched client")
+
+	file, err := os.Open(payloadFolder + payloadFileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Obtenez la taille du fichier
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	size := fileInfo.Size()
+
+	// Get file extension
+	fileExtension := filepath.Ext(fileInfo.Name())
+
+	fmt.Printf("Size: %d\n", size)
+	fmt.Printf("Extension: %s\n", fileExtension)
+
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println(err)
@@ -14,14 +49,80 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connected to server")
-	for i := 1; i <= 1024; i++ {
-		buf := make([]byte, 1024)
-		n, err := conn.Read(buf)
-		if err != nil {
+
+	packetCount := int64(math.Ceil(float64(size) / float64(packetSize)))
+	fmt.Printf("Packet count: %d\n", packetCount)
+
+	packetCountBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(packetCountBytes, uint64(packetCount))
+
+	extensionSizeBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(extensionSizeBytes, uint64(len(fileExtension)))
+
+	fileExtensionBytes := []byte(fileExtension)
+
+	fmt.Printf("Packet count bytes: %v\n", packetCountBytes)
+	fmt.Printf("Extension size bytes: %v\n", extensionSizeBytes)
+	fmt.Printf("Extension bytes: %v\n", fileExtensionBytes)
+
+	fmt.Println("Sending payload size")
+	_, err = conn.Write(packetCountBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Sending file extension size")
+	_, err = conn.Write(extensionSizeBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Sending file extension")
+	_, err = conn.Write(fileExtensionBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Create buffer to read file
+	buf := make([]byte, packetSize)
+
+	for i := int64(1); i <= packetCount; i++ {
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(buf[:n])
+
+		// fmt.Printf("Read %d bytes\n", n)
+		// fmt.Printf("Read %d \n", buf[:n])
+
+		_, err = conn.Write(buf[:n])
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		//Print progress bar
+		fmt.Printf("%d/%d\n", i, packetCount)
+		// Print percentage
+		fmt.Printf("%d%%\n", int(float64(i)/float64(packetCount)*100))
+
+		// Go back 2 lines and erase them
+		if i < packetCount {
+			fmt.Printf("\033[2A")
+		}
 	}
+
+	fmt.Println("Done sending payload")
+
+	message := make([]byte, 1024)
+	n, err := conn.Read(message)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(message[:n]))
 
 }
