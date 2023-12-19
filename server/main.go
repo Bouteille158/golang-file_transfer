c581e8a18c4e6242eceadd4fef71b97ef5893c30
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2023-12-19 14:32:22 +0100

    feat(server): receive file data and write to output directory

--- server/main.go
+++ server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 )
+
+var outputFolder = "./reception"
+var packetSize = 1024
 
 func main() {
 	fmt.Println("Launched server")
@@ -29,16 +34,73 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	fmt.Println("Client connected")
-	// Loop, print 1024 times numbers from 1 to 1024 with a wait of 1 second between each print and then send back a message to the client
-	for i := 1; i <= 1024; i++ {
-		fmt.Println(i)
-		_, err := conn.Write([]byte(fmt.Sprintf("%d", i)))
+
+	payloadSizeBytes := make([]byte, 8)
+	_, err := conn.Read(payloadSizeBytes)
+	if err != nil {
+		log.Fatal("Error while reading payload size:\n", err)
+		return
+	}
+
+	extensionSizeBytes := make([]byte, 8)
+	_, err = conn.Read(extensionSizeBytes)
+	if err != nil {
+		log.Fatal("Error while reading extension size:\n", err)
+		return
+	}
+
+	extensionSize := int64(binary.BigEndian.Uint64(extensionSizeBytes))
+
+	fileExtensionBytes := make([]byte, extensionSize)
+	_, err = conn.Read(fileExtensionBytes)
+	if err != nil {
+		log.Fatal("Error while reading file extension:\n", err)
+		return
+	}
+
+	fmt.Printf("Payload size bytes: %v\n", payloadSizeBytes)
+	// Convert payload size bytes to int64
+	payloadSize := int64(binary.BigEndian.Uint64(payloadSizeBytes))
+	fmt.Printf("Payload size: %d\n", payloadSize)
+
+	outputFileName := time.Now().Format("2006-01-02_15-04-05") + "." + string(fileExtensionBytes)
+
+	// Create output folder if it doesn't exist
+	if _, err := os.Stat(outputFolder); os.IsNotExist(err) {
+		err = os.Mkdir(outputFolder, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//Create output file
+	_, err = os.Create(outputFolder + "/" + outputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := int64(1); i <= payloadSize; i++ {
+		buf := make([]byte, packetSize)
+		n, err := conn.Read(buf)
 		if err != nil {
 			log.Fatal(err)
 			break
 		}
-		time.Sleep(time.Second)
+		fmt.Print(buf[:n])
+
+		f, err := os.OpenFile(outputFolder+"/"+outputFileName, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if _, err := f.Write(buf[:n]); err != nil {
+			log.Fatal(err)
+		}
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
 	}
+	fmt.Println("\nDone reading payload")
 	conn.Write([]byte("I'm done"))
 
 }
